47d51f969d1542ff55a1e46bcc417033ab959aad
MaiaraMorit <maiaramorit@gmail.com>
2022-12-07 19:46:40 -0300

    seção 5 - exercício 2 - mudança de cor

--- FUNDAMENTOS/seção 5 - Introdução JavaScript/scriptseção 5 - Exercícios - funções de manipulação do DOM.js
+++ FUNDAMENTOS/seção 5 - Introdução JavaScript/scriptseção 5 - Exercícios - funções de manipulação do DOM.js
@@ -1,7 +1,17 @@
-function goals () {
+//Crie e execute uma função que mude o texto na tag <p>-----</p>, para uma descrição de como você se vê daqui a 2 anos;
+
+function goals() {
     let paragraph = document.getElementsByTagName('p')[1];
     console.log(paragraph);          
-    paragraph.innerText = 'HEY';
+    paragraph.innerText = 'Me tornar desenvolvedora!';
 }
 
 goals()
+
+
+function color() {
+    let changeColor = document.getElementsByClassName('main-content')[0];
+    changeColor.style.background = 'rgb(76,164,109)';
+}
+
+color()
